e299c4ab40be72a1fcb20a5083c5ba45752fd7ab
daniellobatonUNAM <danielmedina117@comunidad.unam.mx>
2023-05-13 09:59:39 -0600

    Vistas básicas del proyecto

---
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.proyectofinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnAdd: ImageButton
+    private lateinit var btnBoard: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAdd = findViewById(R.id.btnAdd)
+        btnBoard = findViewById(R.id.btnBoard)
+        pasarAddTarea()
+        pasarTablero()
+
+    }
+
+    private fun pasarAddTarea(){
+        btnAdd.setOnClickListener {
+            val intent = Intent(this, AddTarea::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun pasarTablero(){
+        btnBoard.setOnClickListener {
+            val intent = Intent(this, Tablero::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
